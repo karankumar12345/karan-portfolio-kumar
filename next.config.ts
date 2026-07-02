f25dfa60ac0ceb1ef92c66f7f30819ef54875307
karan1_2 <karankumar2004122@gmail.com>
2026-07-02 16:30:46 +0530

    build error

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    /* config options here */
-    eslint:{
-      ignoreDuringBuilds:false,
-    },
-    typescript:{
-      ignoreBuildErrors:true
-    }
-    ,  experimental: {
-      serverActions: {}// Enables server actions
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
